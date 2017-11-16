d19c90a82cba5534555214a8ca5129e6d66f3790
Francesco Gianni <francesco.gianni@objectway.it>
2017-11-16 16:03:41 +0100

    Spring boot!!!

---
+++ stage-java-configuration-application/src/main/java/com/objectway/stage/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.objectway.stage.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hi!";
+    }
+
+
+}
